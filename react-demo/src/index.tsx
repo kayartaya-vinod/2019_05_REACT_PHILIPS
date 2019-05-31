86d537e4829ddfaffdf0c7bfe93c6fa4c7efdf79
kayartaya-vinod <vinod@vinod.co>
2019-05-31 09:20:16 +0530

    🇮🇳

--- react-demo/src/index.tsx
+++ react-demo/src/index.tsx
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
-import App from './demo5/App';
+import App from './sandbox/App';
 // const render = require('react-dom').render;
 
 render(<App />, document.getElementById('root'));
